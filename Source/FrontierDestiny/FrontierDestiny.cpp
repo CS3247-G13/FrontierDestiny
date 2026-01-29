60030440d59858e96c28d409e3ad714d355fd575
Izn432 <isaac.ng432@gmail.com>
2026-01-30 00:33:43 +0800

    Initialize project

---
+++ Source/FrontierDestiny/FrontierDestiny.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FrontierDestiny.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FrontierDestiny, "FrontierDestiny" );
