5560029609932a4754216dff6399c93edb56cd98
IzN432 <isaac.ng432@gmail.com>
2026-02-13 11:39:58 +0800

    Give enemies default HP of 100

--- Source/FrontierDestiny/Public/EnemyActor.h
+++ Source/FrontierDestiny/Public/EnemyActor.h
@@ -21,10 +21,10 @@ public:
 	FOnEnemyDied OnEnemyDied;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Properties")
-	int32 MaxHealth;
+	int32 MaxHealth = 100;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Properties")
-	int32 CurrentHealth;
+	int32 CurrentHealth = 100;
 
 	UFUNCTION(BlueprintCallable, Category = "Enemy Functions")
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
