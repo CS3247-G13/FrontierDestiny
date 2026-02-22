069e2972b1d02b0d7db155a6709955041dcd094a
Izn432 <isaac.ng432@gmail.com>
2026-02-22 12:41:38 +0800

    Add shooting

--- Source/FrontierDestiny/Private/CombatComponent.cpp
+++ Source/FrontierDestiny/Private/CombatComponent.cpp
@@ -20,7 +20,22 @@ void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActo
 {
 	CurrentCooldown = FMath::Max(0.f, CurrentCooldown - DeltaTime);
 
-	RecoverRecoil(DeltaTime);
+	switch (CurrentWeapon)
+	{
+	case EWeaponType::Rifle:
+		RifleRecoverRecoil(DeltaTime);
+		break;
+	case EWeaponType::Shotgun:
+		ShotgunRecoverRecoil(DeltaTime);
+		break;
+	}
+}
+
+void UCombatComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	GetOwner()->GetWorldTimerManager().ClearTimer(ReplenishBulletTimerHandle);
 }
 
 void UCombatComponent::SetupInput(UInputComponent* InputComponent)
@@ -45,13 +60,22 @@ void UCombatComponent::SetupInput(UInputComponent* InputComponent)
 				&UCombatComponent::OnTriggerStartAction
 			);
 		}
-		if (ReloadAction)
+		if (SwapWeaponAction)
 		{
 			EnhancedInputComponent->BindAction(
-				ReloadAction,
+				SwapWeaponAction,
 				ETriggerEvent::Triggered,
 				this,
-				&UCombatComponent::OnReloadAction
+				&UCombatComponent::OnSwapWeaponAction
+			);
+		}
+		if (SelectWeaponAction)
+		{
+			EnhancedInputComponent->BindAction(
+				SelectWeaponAction,
+				ETriggerEvent::Triggered,
+				this,
+				&UCombatComponent::OnSelectWeaponAction
 			);
 		}
 	}
@@ -62,11 +86,36 @@ void UCombatComponent::BeginPlay()
 	Super::BeginPlay();
 
 	CurrentBullets = MaxBullets;
+	
+
+	GetOwner()->GetWorldTimerManager().SetTimer(
+		ReplenishBulletTimerHandle,
+		this,
+		&UCombatComponent::ReplenishBullet,
+		BulletReplenishCooldown,
+		true
+	);
+}
+
+void UCombatComponent::ReplenishBullet()
+{
+	CurrentBullets = FMath::Min(CurrentBullets + 1, MaxBullets);
 }
 
 void UCombatComponent::OnTriggerStartAction(const FInputActionValue& Value)
 {
-	if (CurrentBullets == 0)
+	int32 BulletCost;
+	switch (CurrentWeapon)
+	{
+	case EWeaponType::Rifle:
+		BulletCost = RifleBulletCost;
+		break;
+	case EWeaponType::Shotgun:
+		BulletCost = ShotgunBulletCost;
+		break;
+	}
+
+	if (CurrentBullets < BulletCost)
 	{
 		if (OutOfBulletsSound)
 		{
@@ -75,18 +124,36 @@ void UCombatComponent::OnTriggerStartAction(const FInputActionValue& Value)
 	}
 }
 
-void UCombatComponent::OnReloadAction(const FInputActionValue& Value)
+void UCombatComponent::OnSwapWeaponAction(const FInputActionValue& Value)
 {
-	PerformReload();
+	SwapWeapon();
 }
 
-void UCombatComponent::PerformReload()
+void UCombatComponent::OnSelectWeaponAction(const FInputActionValue& Value)
 {
-	if (ReloadSound)
-	{
-		UGameplayStatics::PlaySound2D(GetWorld(), ReloadSound);
-	}
-	CurrentBullets = MaxBullets;
+	int32 InputValue = FMath::RoundToInt32(Value.Get<float>());
+	uint8 WeaponIndex = static_cast<uint8>(FMath::Max(0, (InputValue - 1) % 2));
+	SelectWeapon(static_cast<EWeaponType>(WeaponIndex));
+}
+
+void UCombatComponent::SwapWeapon()
+{
+	SelectWeapon(static_cast<EWeaponType>((static_cast<uint8>(CurrentWeapon) + 1) % 2));
+}
+
+void UCombatComponent::SelectWeapon(EWeaponType WeaponType)
+{
+	// Supposed to play an animation and use a callback to reenable the shooting but here we are
+	CurrentWeapon = WeaponType;
+	CurrentCooldown = 0;
+
+	// 1. Get the Enum as a UEnum pointer
+	const UEnum* EnumPtr = StaticEnum<EWeaponType>();
+
+	// 2. Convert the current value to a FString, then to TCHAR* for the log
+	FString WeaponName = EnumPtr->GetValueAsString(CurrentWeapon);
+
+	UE_LOG(LogTemp, Display, TEXT("Changed weapon to %s"), *WeaponName);
 }
 
 void UCombatComponent::OnTriggerAction(const FInputActionValue& Value)
@@ -97,26 +164,46 @@ void UCombatComponent::OnTriggerAction(const FInputActionValue& Value)
 		return;
 	}
 
-	// Check if out of bullets
-	if (CurrentBullets == 0)
+	// code duplication trigger warning:
+	switch (CurrentWeapon)
 	{
-		return;
+	case EWeaponType::Rifle:
+		if (CurrentBullets < RifleBulletCost)
+		{
+			return;
+		}
+		PerformRifleShoot();
+		CurrentBullets -= RifleBulletCost;
+
+		if (RifleTriggerSound)
+		{
+			UGameplayStatics::PlaySound2D(GetWorld(), RifleTriggerSound);
+		}
+
+		RifleRecoil();
+
+		CurrentCooldown = 1.f / RifleFireRate;
+		break;
+	case EWeaponType::Shotgun:
+		if (CurrentBullets < ShotgunBulletCost)
+		{
+			return;
+		}
+
+		PerformShotgunShoot();
+		CurrentBullets -= ShotgunBulletCost;
+
+		if (ShotgunTriggerSound)
+		{
+			UGameplayStatics::PlaySound2D(GetWorld(), ShotgunTriggerSound);
+		}
+
+		CurrentCooldown = 1.f / ShotgunFireRate;
+		break;
 	}
-
-	PerformShoot();
-	Recoil();
-
-	CurrentBullets -= 1;
-
-	if (TriggerSound)
-	{
-		UGameplayStatics::PlaySound2D(GetWorld(), TriggerSound);
-	}
-
-	CurrentCooldown = 1.f / FireRate;
 }
 
-void UCombatComponent::PerformShoot()
+void UCombatComponent::PerformRifleShoot()
 {
 	FHitResult Hit;
 	FVector TraceStart = Camera->GetComponentLocation();
@@ -127,51 +214,83 @@ void UCombatComponent::PerformShoot()
 	FVector Direction = Camera->GetForwardVector();
 	Direction = HorizontalRecoilRotator.RotateVector(Direction);
 
-	FVector TraceEnd = TraceStart + Direction * Range;
+	FVector TraceEnd = TraceStart + Direction * RifleRange;
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(Pawn);
 	GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ShootingTargetChannel, QueryParams);
-	// DrawDebugLine(GetWorld(), TraceStart, TraceEnd, Hit.bBlockingHit ? FColor::Blue : FColor::Red, false, 5.0f, 0, 1.0f);
 
 	if (Hit.bBlockingHit && IsValid(Hit.GetActor()))
 	{
 		FTransform SpawnTransform((-Direction).Rotation(), TraceStart + Direction * 100.f);
-
-		/*ADamageNumber* NewDamageNumber = GetWorld()->SpawnActorDeferred<ADamageNumber>(
-			ADamageNumber::StaticClass(),
-			SpawnTransform,
-			GetOwner(),
-			Pawn,
-			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
-		);
-
-		if (NewDamageNumber)
-		{
-			NewDamageNumber->DamageNumber = 50;
-			NewDamageNumber->FinishSpawning(SpawnTransform);
-		}*/
-
 		DrawDebugBox(GetWorld(), Hit.Location, FVector(5.f, 5.f, 5.f), FColor::Red, true, 1.f, 0, 1.f);
 	}
 }
 
-void UCombatComponent::Recoil()
+void UCombatComponent::PerformShotgunShoot()
+{
+	FHitResult Hit;
+	FVector TraceStart = Camera->GetComponentLocation();
+	FVector Direction = Camera->GetForwardVector();
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(Pawn);
+
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			FRotator ShotgunSpreadRotator;
+			ShotgunSpreadRotator.Yaw = i * ShotgunSpreadAngle;
+			ShotgunSpreadRotator.Pitch = j * ShotgunSpreadAngle;
+			
+			FVector BulletDirection = ShotgunSpreadRotator.RotateVector(Direction);
+			FVector TraceEnd = TraceStart + BulletDirection * ShotgunRange;
+
+			GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ShootingTargetChannel, QueryParams);
+
+			if (Hit.bBlockingHit && IsValid(Hit.GetActor()))
+			{
+				FTransform SpawnTransform((-Direction).Rotation(), TraceStart + Direction * 100.f);
+				DrawDebugBox(GetWorld(), Hit.Location, FVector(5.f, 5.f, 5.f), FColor::Red, true, 1.f, 0, 1.f);
+			}
+		}
+	}
+
+}
+
+void UCombatComponent::RifleRecoil()
 {
 	int Sign = FMath::RandBool() ? 1 : -1;
-	FVector2D NextOffset = CurrentOffset + FVector2D(RecoilAmount.X * Sign, RecoilAmount.Y);
-	NextOffset.X = FMath::Min(NextOffset.X, MaxRecoilAmount.X);
-	NextOffset.Y = FMath::Min(NextOffset.Y, MaxRecoilAmount.Y);
+	FVector2D NextOffset = CurrentOffset + FVector2D(RifleRecoilAmount.X * Sign, RifleRecoilAmount.Y);
+	NextOffset.X = FMath::Min(NextOffset.X, RifleMaxRecoilAmount.X);
+	NextOffset.Y = FMath::Min(NextOffset.Y, RifleMaxRecoilAmount.Y);
 	FVector2D DeltaOffset = NextOffset - CurrentOffset;
-	// Pawn->AddControllerYawInput(-DeltaOffset.X);
 	Pawn->AddControllerPitchInput(-DeltaOffset.Y);
 	CurrentOffset = NextOffset;
 }
 
-void UCombatComponent::RecoverRecoil(float DeltaSeconds)
+void UCombatComponent::RifleRecoverRecoil(float DeltaSeconds)
 {
-	float DeltaOffsetX = FMath::Min(CurrentOffset.X, OffsetRecoverySpeed.X * DeltaSeconds);
-	float DeltaOffsetY = FMath::Min(CurrentOffset.Y, OffsetRecoverySpeed.Y * DeltaSeconds);
+	float DeltaOffsetX = FMath::Min(CurrentOffset.X, RifleOffsetRecoverySpeed.X * DeltaSeconds);
+	float DeltaOffsetY = FMath::Min(CurrentOffset.Y, RifleOffsetRecoverySpeed.Y * DeltaSeconds);
 	CurrentOffset -= FVector2D(DeltaOffsetX, DeltaOffsetY);
-	// Pawn->AddControllerYawInput(DeltaOffsetX);
+	Pawn->AddControllerPitchInput(DeltaOffsetY);
+}
+
+void UCombatComponent::ShotgunRecoil()
+{
+	int Sign = FMath::RandBool() ? 1 : -1;
+	FVector2D NextOffset = CurrentOffset + FVector2D(ShotgunRecoilAmount.X * Sign, ShotgunRecoilAmount.Y);
+	NextOffset.X = FMath::Min(NextOffset.X, ShotgunMaxRecoilAmount.X);
+	NextOffset.Y = FMath::Min(NextOffset.Y, ShotgunMaxRecoilAmount.Y);
+	FVector2D DeltaOffset = NextOffset - CurrentOffset;
+	Pawn->AddControllerPitchInput(-DeltaOffset.Y);
+	CurrentOffset = NextOffset;
+}
+
+void UCombatComponent::ShotgunRecoverRecoil(float DeltaSeconds)
+{
+	float DeltaOffsetX = FMath::Min(CurrentOffset.X, ShotgunOffsetRecoverySpeed.X * DeltaSeconds);
+	float DeltaOffsetY = FMath::Min(CurrentOffset.Y, ShotgunOffsetRecoverySpeed.Y * DeltaSeconds);
+	CurrentOffset -= FVector2D(DeltaOffsetX, DeltaOffsetY);
 	Pawn->AddControllerPitchInput(DeltaOffsetY);
 }
--- Source/FrontierDestiny/Public/CombatComponent.h
+++ Source/FrontierDestiny/Public/CombatComponent.h
@@ -10,67 +10,116 @@
 
 class UInputAction;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	Rifle	UMETA(DisplayName = "Rifle"),
+	Shotgun	UMETA(DisplayName = "Shotgun")
+};
+
+// This is for changing the cross hair when the weapon changes
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWeaponChange, EWeaponType, Weapon);
+
+// This is for updating the HUD with the number of bullets left
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnCurrentBulletsChange, int32, CurrentBullets, int32, MaxBullets);
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class FRONTIERDESTINY_API UCombatComponent : public UModeComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UCombatComponent();
 
 	virtual void SetupInput(UInputComponent* InputComponent) override;
 	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 protected:
-	
+	UPROPERTY(BlueprintAssignable, Category = "Combat|Events")
+	FOnWeaponChange OnWeaponChange;
+	UPROPERTY(BlueprintAssignable, Category = "Combat|Events")
+	FOnCurrentBulletsChange OnCurrentBulletsChange;
+
+	void ReplenishBullet();
+
 	UPROPERTY(EditAnywhere, Category = "Setup")
 	TObjectPtr<UInputAction> TriggerAction;
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	TObjectPtr<UInputAction> ReloadAction;
 	UPROPERTY(EditAnywhere, Category = "Setup")
 	TObjectPtr<UInputAction> SwapWeaponAction;
 	UPROPERTY(EditAnywhere, Category = "Setup")
 	TObjectPtr<UInputAction> SelectWeaponAction;
 
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	TObjectPtr<USoundBase> TriggerSound;
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float BulletReplenishCooldown = 0.3f;
+
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	TObjectPtr<USoundBase> RifleTriggerSound;
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	TObjectPtr<USoundBase> ShotgunTriggerSound;
+
 	UPROPERTY(EditAnywhere, Category = "Setup")
 	TObjectPtr<USoundBase> OutOfBulletsSound;
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	TObjectPtr<USoundBase> ReloadSound;
 
 	UFUNCTION()
 	void OnTriggerAction(const FInputActionValue& Value);
 	UFUNCTION()
 	void OnTriggerStartAction(const FInputActionValue& Value);
 	UFUNCTION()
-	void OnReloadAction(const FInputActionValue& Value);
+	void OnSwapWeaponAction(const FInputActionValue& Value);
 	UFUNCTION()
-	void Recoil();
-	UFUNCTION()
-	void RecoverRecoil(float DeltaTime);
-	UFUNCTION()
-	void PerformShoot();
-	UFUNCTION()
-	void PerformReload();
+	void OnSelectWeaponAction(const FInputActionValue& Value);
+
+	// Code duplication intensifies
+	void RifleRecoil();
+	void RifleRecoverRecoil(float DeltaTime);
+
+	void ShotgunRecoil();
+	void ShotgunRecoverRecoil(float DeltaTime);
+
+	void PerformRifleShoot();
+	void PerformShotgunShoot();
+
+	void SwapWeapon();
+	void SelectWeapon(EWeaponType WeaponType);
 
 	UPROPERTY(EditAnywhere, Category = "Setup")
 	TEnumAsByte<ECollisionChannel> ShootingTargetChannel = ECC_Pawn;
 	UPROPERTY(EditAnywhere, Category = "Setup")
-	float FireRate = 3.333333333333333f;
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	FVector2D RecoilAmount { 0.0f, 0.4f };
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	FVector2D MaxRecoilAmount { 0.0f, 1.f };
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	FVector2D OffsetRecoverySpeed { 1.f, 3.f };
-	UPROPERTY(EditAnywhere, Category = "Setup")
-	float Range = 10000.f;
-	UPROPERTY(EditAnywhere, Category = "Setup")
 	int32 MaxBullets = 32;
 
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	float RifleFireRate = 3.333333333333333f;
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	FVector2D RifleRecoilAmount{ 0.0f, 0.4f };
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	FVector2D RifleMaxRecoilAmount{ 0.0f, 1.f };
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	FVector2D RifleOffsetRecoverySpeed{ 1.f, 3.f };
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	float RifleRange = 10000.f;
+	UPROPERTY(EditAnywhere, Category = "Setup|Rifle")
+	int32 RifleBulletCost = 1;
+
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	float ShotgunFireRate = 1.f;
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	FVector2D ShotgunRecoilAmount{ 0.0f, 5.f };
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	FVector2D ShotgunMaxRecoilAmount{ 0.0f, 10.f };
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	FVector2D ShotgunOffsetRecoverySpeed{ 0.f,};
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	float ShotgunRange = 10000.f;
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	float ShotgunSpreadAngle = 1.5f;
+	UPROPERTY(EditAnywhere, Category = "Setup|Shotgun")
+	int32 ShotgunBulletCost = 9;
+
+	UPROPERTY(VisibleAnywhere, Category = "Debug")
+	EWeaponType CurrentWeapon = EWeaponType::Rifle;
 	UPROPERTY(VisibleAnywhere, Category = "Debug")
 	int32 CurrentBullets;
 	UPROPERTY(VisibleAnywhere, Category = "Debug")
@@ -78,7 +127,5 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Debug")
 	FVector2D CurrentOffset;
 	UPROPERTY(VisibleAnywhere, Category = "Debug")
-	float FireDuration;
-	UPROPERTY(VisibleAnywhere, Category = "Debug")
-	bool bIsFiring = false;
+	FTimerHandle ReplenishBulletTimerHandle;
 };
